b0abdc8f7bcbf4e01093116a39fc90feea8e1d27
Mohd Hareem <mdhareem0786@gmail.com>
2025-11-14 18:24:42 +0530

    4th mini project is done

---
+++ Digital Clock/script.js
@@ -0,0 +1,16 @@
+const timeDiv = document.getElementById("time");
+const dateDiv = document.getElementById("date")
+
+
+setInterval(() =>{
+let time = new Date();
+timeDiv.textContent = time.toLocaleTimeString();
+
+dateDiv.textContent = time.toLocaleDateString("en-In",{
+    weekday: "long",
+    day:"2-digit",
+    month: "long",
+    year: "numeric"
+})
+
+},1000)
